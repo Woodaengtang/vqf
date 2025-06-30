7a5b9ccce97527812ea4785727f55b9f922e6bd8
Woodaengtang <jhwoo200@gmail.com>
2025-07-01 00:13:04 +0900

    real last update before meeting

--- vqf/matlab/bias_plot.m
+++ vqf/matlab/bias_plot.m
@@ -1,23 +1,39 @@
 close all; clear all; clc;
-biasData = readtable("log\Huro_06_30_1432.csv");
-    
+biasData = readtable("log\Huro_06_30_1222.csv");
+
+gry_norm = NaN([length(biasData.debug0), 1]);
 bias_norm = NaN([length(biasData.debug0), 1]);
 for i = 1:length(bias_norm)
     bias_norm(i) = norm([biasData.debug0(i), biasData.debug1(i), biasData.debug2(i)]);
+    gry_norm(i) = norm([biasData.gx(i), biasData.gy(i), biasData.gz(i)]);
 end
 
 ts = 1/570;
 time = linspace(ts, ts*length(bias_norm), length(bias_norm));
 
+rmse = [norm(mean(biasData.gx) - biasData.debug0), norm(mean(biasData.gy) - biasData.debug1), norm(mean(biasData.gz) - biasData.debug2)];
+
 rad2deg = 180/pi;
 biasPlot = figure();
+biasPlot.Position = [1000, 106, 792, 1132];
+subplot(4, 1, 1);
 hold on; grid on;
-% biasNorm = plot(time, bias_norm.*rad2deg, "LineWidth", 1);
-% biasRol = plot(time, biasData.debug0.*rad2deg, "LineWidth", 1);
-% biasPit = plot(time, biasData.debug1.*rad2deg, "LineWidth", 1);
-% biasYaw = plot(time, biasData.debug2.*rad2deg, "LineWidth", 1);
-biasNorm = plot(time, bias_norm , "LineWidth", 1);
-biasRol = plot(time, biasData.debug0 , "LineWidth", 1);
-biasPit = plot(time, biasData.debug1 , "LineWidth", 1);
-biasYaw = plot(time, biasData.debug2 , "LineWidth", 1);
-legend([biasNorm, biasRol, biasPit, biasYaw], {"Norm", "Rol", "Pit", "Yaw"});
+normGryRaw = plot(time, gry_norm);
+biasNorm = plot(time, bias_norm, 'LineWidth', 2, 'LineStyle', '--');
+legend([normGryRaw, biasNorm], {"Raw data", "Estimated bias"});
+ylabel("Gry norm");
+subplot(4, 1, 2);
+hold on; grid on;
+gxRaw = plot(time, biasData.gx);
+biasRol = plot(time, biasData.debug0, 'LineWidth', 2, 'LineStyle', '--');
+ylabel("G_x");
+subplot(4, 1, 3);
+hold on; grid on;
+gyRaw = plot(time, biasData.gy);
+biasPit = plot(time, biasData.debug1, 'LineWidth', 2, 'LineStyle', '--');
+ylabel("G_y");
+subplot(4, 1, 4);
+hold on; grid on;
+gzRaw = plot(time, biasData.gz);
+biasYaw = plot(time, biasData.debug2, 'LineWidth', 2, 'LineStyle', '--');
+ylabel("G_z"); xlabel("time (s)");
--- vqf/matlab/mag_bias_test.m
+++ vqf/matlab/mag_bias_test.m
@@ -13,7 +13,7 @@ moveidx = NaN([2, 1]);
 for i = 1:length(rawData.Rest)
     rest_idx = rawData.Rest(i);
     if ~(rest_idx == rest_idx_prev)
-        moveidx(count + 1) = i;
+        moveidx(count + 1) = i-1;
         if count == 0
             init_mx_mean = mean(rawData.mx(1:i-1));
             init_my_mean = mean(rawData.my(1:i-1));
@@ -35,15 +35,63 @@ for i = 1:length(rawData.Rest)
 end
 
 biasCheck = figure();
-subplot([3, 1, 1]);
+biasCheck.Position = [1000, 99, 839, 1139];
+subplot(3, 1, 1);
 grid on; hold on;
-plot(time, rawData.mx, 'LineWidth', 1);
+y_lims_init = [init_mx_mean-init_mx_std*3, init_mx_mean+init_mx_std*3];
+y_coords_init = [y_lims_init(1), y_lims_init(1), y_lims_init(2), y_lims_init(2)];
+x_patch1 = [time(1), time(moveidx(1)), time(moveidx(1)), time(1)];
+patch(x_patch1, y_coords_init, 'r', 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+y_lims_mv = [mv_mx_mean-mv_mx_std*3, mv_mx_mean+mv_mx_std*3];
+y_coords_init = [y_lims_mv(1), y_lims_mv(1), y_lims_mv(2), y_lims_mv(2)];
+x_patch2 = [time(moveidx(2)), time(end), time(end), time(moveidx(2))];
+patch(x_patch2, y_coords_init, 'r', 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+plot(time, rawData.mx);
+plot(time(1:moveidx(1)), init_mx_mean*ones([moveidx(1), 1]), 'r--', 'LineWidth', 2);
+plot(time(moveidx(2):end), mv_mx_mean*ones([length(time)-moveidx(2)+1, 1]), 'r--', 'LineWidth', 2);
+y_lims = ylim;
+y_coords= [y_lims(1), y_lims(1), y_lims(2), y_lims(2)];
+x_patch = [time(moveidx(1)), time(moveidx(2)), time(moveidx(2)), time(moveidx(1))];
+patch(x_patch, y_coords, 'g', 'FaceAlpha', 0.1, 'EdgeColor', 'none');
+xlabel('time (s)'); ylabel("mx");
 
-subplot([3, 1, 2]);
+subplot(3, 1, 2);
 grid on; hold on;
-plot(time, rawData.my, 'LineWidth', 1);
-subplot([3, 1, 3]);
-plot(time, rawData.mz, 'LineWidth', 1);
+y_lims_init = [init_my_mean-init_my_std*3, init_my_mean+init_my_std*3];
+y_coords_init = [y_lims_init(1), y_lims_init(1), y_lims_init(2), y_lims_init(2)];
+x_patch1 = [time(1), time(moveidx(1)), time(moveidx(1)), time(1)];
+patch(x_patch1, y_coords_init, 'r', 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+y_lims_mv = [mv_my_mean-mv_my_std*3, mv_my_mean+mv_my_std*3];
+y_coords_init = [y_lims_mv(1), y_lims_mv(1), y_lims_mv(2), y_lims_mv(2)];
+x_patch2 = [time(moveidx(2)), time(end), time(end), time(moveidx(2))];
+patch(x_patch2, y_coords_init, 'r', 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+plot(time, rawData.my);
+plot(time(1:moveidx(1)), init_my_mean*ones([moveidx(1), 1]), 'r--', 'LineWidth', 1);
+plot(time(moveidx(2):end), mv_my_mean*ones([length(time)-moveidx(2)+1, 1]), 'r--', 'LineWidth', 2);
+y_lims = ylim;
+y_coords= [y_lims(1), y_lims(1), y_lims(2), y_lims(2)];
+x_patch = [time(moveidx(1)), time(moveidx(2)), time(moveidx(2)), time(moveidx(1))];
+patch(x_patch, y_coords, 'g', 'FaceAlpha', 0.1, 'EdgeColor', 'none');
+xlabel('time (s)'); ylabel("my");
+
+subplot(3, 1, 3);
+grid on; hold on;
+y_lims_init = [init_mz_mean-init_mz_std*3, init_mz_mean+init_mz_std*3];
+y_coords_init = [y_lims_init(1), y_lims_init(1), y_lims_init(2), y_lims_init(2)];
+x_patch1 = [time(1), time(moveidx(1)), time(moveidx(1)), time(1)];
+patch(x_patch1, y_coords_init, 'r', 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+y_lims_mv = [mv_mz_mean-mv_mz_std*3, mv_mz_mean+mv_mz_std*3];
+y_coords_init = [y_lims_mv(1), y_lims_mv(1), y_lims_mv(2), y_lims_mv(2)];
+x_patch2 = [time(moveidx(2)), time(end), time(end), time(moveidx(2))];
+patch(x_patch2, y_coords_init, 'r', 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+plot(time, rawData.mz);
+plot(time(1:moveidx(1)), init_mz_mean*ones([moveidx(1), 1]), 'r--', 'LineWidth', 1);
+plot(time(moveidx(2):end), mv_mz_mean*ones([length(time)-moveidx(2)+1, 1]), 'r--', 'LineWidth', 2);
+y_lims = ylim;
+y_coords= [y_lims(1), y_lims(1), y_lims(2), y_lims(2)];
+x_patch = [time(moveidx(1)), time(moveidx(2)), time(moveidx(2)), time(moveidx(1))];
+patch(x_patch, y_coords, 'g', 'FaceAlpha', 0.1, 'EdgeColor', 'none');
+xlabel('time (s)'); ylabel("mz");
 
 init_mag = [init_mx_mean, init_my_mean, init_mz_mean, init_mx_std, init_my_std, init_mz_std];
 mv_mag = [mv_mx_mean, mv_my_mean, mv_mz_mean, mv_mx_std, mv_my_std, mv_mz_std];
